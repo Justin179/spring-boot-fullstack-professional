4ac5beee03c10e4820813b3aa90c2224b1a379f0
Justin-Chen <justin.uob@gmail.com>
2021-11-23 17:46:04 +0800

    61%

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -144,7 +144,19 @@ function App() {
             return <Spin indicator={antIcon}/>
         }
         if (students.length <= 0) {
-            return <Empty/>;
+            return <>
+                <Button
+                    onClick={() => setShowDrawer(!showDrawer)}
+                    type="primary" shape="round" icon={<PlusOutlined/>} size="small">
+                    Add New Student
+                </Button>
+                <StudentDrawerForm
+                    showDrawer={showDrawer}
+                    setShowDrawer={setShowDrawer}
+                    fetchStudents={fetchStudents}
+                />
+                <Empty/>
+            </>
         }
         return <>
             <StudentDrawerForm
@@ -175,10 +187,13 @@ function App() {
         </>
     }
 
+
+
     return <Layout style={{minHeight: '100vh'}}>
         <Sider collapsible collapsed={collapsed}
                onCollapse={setCollapsed}>
             <div className="logo"/>
+
             <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
                 <Menu.Item key="1" icon={<PieChartOutlined/>}>
                     Option 1
--- src/main/java/com/example/demo/student/Student.java
+++ src/main/java/com/example/demo/student/Student.java
@@ -3,6 +3,7 @@ package com.example.demo.student;
 import lombok.*;
 
 import javax.persistence.*;
+import javax.validation.constraints.*;
 
 @ToString
 @Getter
@@ -23,9 +24,17 @@ public class Student {
             generator = "student_sequence",
             strategy = GenerationType.SEQUENCE)
     private Long id;
+
+    @NotBlank
+    @Column(nullable = false)
     private String name;
+    @Email
+    @Column(nullable = false,unique = true)
     private String email;
+
+    @NotNull
     @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
     private Gender gender;
 
     public Student(String name, String email, Gender gender) {
